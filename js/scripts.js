e02d1a617d503e7a1aea0778ed8de0936df323d3
Omid Ashtari and Slavik Mygovych <omid@>
2016-09-21 20:34:04 -0700

    separate the front and back-end logic in the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,113 @@
+var celebrity = function(age, hairInput, interestsInput, genreInput) {
+  if (age) {
+    if (age >= 50) {
+      alert("Sorry, they're already dead.")
+    } else if ( age >=18 ) {
+      if (hairInput === "brown"){
+        if (interestsInput === "charity") {
+          if (genreInput === "comedy") {
+            return "Aziz Ansari";
+          } else if (genreInput === "drama") {
+            return "George Clooney";
+          } else if (genreInput === "nolan"){
+            return "Ben Affleck";
+          }
+
+        } else if (interestsInput === "dining") {
+          if (genreInput === "comedy") {
+            return "Seth Rogan";
+          } else if (genreInput === "drama") {
+            return "Brad Pitt";
+          } else if (genreInput === "nolan"){
+            return "Matt Damon";
+          }
+
+        } else if (interestsInput === "adventure"){
+          if (genreInput === "comedy") {
+            return "Johnny Depp";
+          } else if (genreInput === "drama") {
+            return "Tom Hanks";
+          } else if (genreInput === "nolan"){
+            return "Tom Cruise";
+          }
+
+        }
+
+      } else if (hairInput === "black") {
+        if (interestsInput === "charity") {
+          if (genreInput === "comedy") {
+              return "Will Smith";
+          } else if (genreInput === "drama") {
+            return "Harrison Ford";
+          } else if (genreInput === "nolan"){
+              return "Morgan Freeman";
+          }
+
+        } else if (interestsInput === "dining") {
+          if (genreInput === "comedy") {
+              return "Clint Eastwood";
+          } else if (genreInput === "drama") {
+            return "Bruce Willis";
+          } else if (genreInput === "nolan"){
+            return "Al Pacino";
+          }
+
+        } else if (interestsInput === "adventure"){
+          if (genreInput === "comedy") {
+            return "Cameron Diaz";
+          } else if (genreInput === "drama") {
+            return "Miley Cyrus";
+          } else if (genreInput === "nolan"){
+              return "Meryl Streep";
+          }
+
+        }
+
+      } else if (hairInput === "red"){
+        if (interestsInput === "charity") {
+          if (genreInput === "comedy") {
+              return "Iron Man";
+          } else if (genreInput === "drama") {
+            return "Ant Man";
+          } else if (genreInput === "nolan"){
+            return "Captain America";
+          }
+
+        } else if (interestsInput === "dining") {
+          if (genreInput === "comedy") {
+            return "Wolverine";
+          } else if (genreInput === "drama") {
+            return "Rogue";
+          } else if (genreInput === "nolan"){
+              return "Thor";
+          }
+
+        } else if (interestsInput === "adventure"){
+          if (genreInput === "comedy") {
+            return "Spider Man";
+          } else if (genreInput === "drama") {
+            return "Super Man";
+          } else if (genreInput === "nolan"){
+            return "Bat Man";
+          }
+
+        }
+      }
+
+
+
+
+    } else {
+      alert("get you lost you PERV!")
+  }
+
+  } else {
+    alert("Please enter your age!")
+  }
+}
+
+
+
 $(document).ready(function() {
 
 
@@ -6,117 +116,12 @@ $(document).ready(function() {
     var hairInput = $('#hair').val();
     var interestsInput = $('input:radio[name=interests]:checked').val();
     var genreInput = $('#genre').val();
-    var locationInput = $('#location').val();
-    var result;
+    var result = celebrity(age, hairInput, interestsInput, genreInput);
     event.preventDefault();
 
 
 
-    if (age) {
-      if (age >= 50) {
-        alert("Sorry, they're already dead.")
-      } else if ( age >=18 ) {
-        if (hairInput === "brown"){
-          if (interestsInput === "charity") {
-            if (genreInput === "comedy") {
-              result = "Aziz Ansari";
-            } else if (genreInput === "drama") {
-              result = "George Clooney";
-            } else if (genreInput === "nolan"){
-              result = "Ben Affleck";
-            }
 
-          } else if (interestsInput === "dining") {
-            if (genreInput === "comedy") {
-              result = "Seth Rogan";
-            } else if (genreInput === "drama") {
-              result = "Brad Pitt";
-            } else if (genreInput === "nolan"){
-              result = "Matt Damon";
-            }
-
-          } else if (interestsInput === "adventure"){
-            if (genreInput === "comedy") {
-              result = "Johnny Depp";
-            } else if (genreInput === "drama") {
-              result = "Tom Hanks";
-            } else if (genreInput === "nolan"){
-              result = "Tom Cruise";
-            }
-
-          }
-
-        } else if (hairInput === "black") {
-          if (interestsInput === "charity") {
-            if (genreInput === "comedy") {
-                result = "Will Smith";
-            } else if (genreInput === "drama") {
-              result = "Harrison Ford";
-            } else if (genreInput === "nolan"){
-                result = "Morgan Freeman";
-            }
-
-          } else if (interestsInput === "dining") {
-            if (genreInput === "comedy") {
-                result = "Clint Eastwood";
-            } else if (genreInput === "drama") {
-              result = "Bruce Willis";
-            } else if (genreInput === "nolan"){
-              result = "Al Pacino";
-            }
-
-          } else if (interestsInput === "adventure"){
-            if (genreInput === "comedy") {
-              result = "Cameron Diaz";
-            } else if (genreInput === "drama") {
-              result = "Miley Cyrus";
-            } else if (genreInput === "nolan"){
-                result = "Meryl Streep";
-            }
-
-          }
-
-        } else if (hairInput === "red"){
-          if (interestsInput === "charity") {
-            if (genreInput === "comedy") {
-                result = "Iron Man";
-            } else if (genreInput === "drama") {
-              result = "Ant Man";
-            } else if (genreInput === "nolan"){
-              result = "Captain America";
-            }
-
-          } else if (interestsInput === "dining") {
-            if (genreInput === "comedy") {
-              result = "Wolverine";
-            } else if (genreInput === "drama") {
-              result = "Rogue";
-            } else if (genreInput === "nolan"){
-                result = "Thor";
-            }
-
-          } else if (interestsInput === "adventure"){
-            if (genreInput === "comedy") {
-              result = "Spider Man";
-            } else if (genreInput === "drama") {
-              result = "Super Man";
-            } else if (genreInput === "nolan"){
-              result = "Bat Man";
-            }
-
-          }
-        }
-
-
-
-
-      } else {
-        alert("get you lost you PERV!")
-    }
-
-    } else {
-      alert("Please enter your age!")
-    }
     $("#result").text(result);
     $("#results").show();
   });
